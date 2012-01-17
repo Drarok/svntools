fff15b712f65db15f4ecf5844f3ea1bc210c4474
Drarok Ithaqua <github@catchall.drarok.com>
2012-01-17 00:55:08 +0000

    Update help to document the new trash command.

--- views/help.php
+++ views/help.php
@@ -4,11 +4,14 @@ NAME
 SYNOPSIS
 	svnstash save <name> [-u|--include-untracked]
 	svnstash list
-	svnstash show [<name>]
-	svnstash apply [<name>]
-	svnstash pop [<name>]
-	svnstash drop [<name>]
+	svnstash show [<name|id>]
+	svnstash apply [<name|id>]
+	svnstash pop [<name|id>]
+	svnstash drop [<name|id>]
 	svnstash clear [-f|--force]
+	svnstash trash [show]
+	svnstash trash empty [-f|--force]
+	svnstash trash restore <name|id> [...]
 	svnstash help
 	
 OPTIONS
@@ -18,18 +21,18 @@ OPTIONS
 		If the --include-untracked option is used, then untracked files are also stashed, and removed from the working copy.
 	
 	list
-		List all stashes in the current working directory.
+		List all stashes in the current working directory, showing their name and id.
 	
-	show [<name>]
+	show [<name|id>]
 		Show the diff from the stash. When no <name> is specified, this command shows the latest stash.
 	
-	apply [<name>]
+	apply [<name|id>]
 		Apply the diff from the stash to the working copy. When no <name> is specified, applies the latest stash.
 	
-	pop [<name>]
+	pop [<name|id>]
 		Apply the diff to the working copy, and remove it afterwards. When no <name> is specified, applies the latest stash.
 	
-	drop [<name>]
+	drop [<name|id>]
 		Delete a stash without applying it to the working copy. When no <name> is specified, removes the latest stash.
 	
 	clear [-f|--force]
@@ -37,5 +40,19 @@ OPTIONS
 		
 		Passing the --force option will not ask for confirmation of this step.
 	
+	trash [show]
+		Shows what's in the trash.
+	
+	trash empty [-f|--force]
+		Permanently delete all the stashes from the trash.
+		
+		Passing the --force option will not ask for confirmation of this step.
+	
+	trash restore <name|id> [...]
+		Restore one or more items from the trash.
+	
 	help
 		This help text.
+
+NOTES
+	Anywhere that svnstash accepts <name|id> style parameters, you can simply use the id shown in the corresponding command.
