973384286dfd26a1e443dd8cd14d5b847921ccf6
Drarok Ithaqua <github@catchall.drarok.com>
2012-05-10 17:00:13 +0100

    Start implementing the reintegrate command.

--- classes/command/svneligible/reintegrate.php
+++ classes/command/svneligible/reintegrate.php
@@ -13,8 +13,9 @@ class Command_Svneligible_Reintegrate extends Command_Svneligible
 	{
 		$svn = new Svn(Svn::getRoot('.'));
 
-		// Don't forget that argument 0 is the command).
-		if (! (bool) $upstreamPath = CLI::getUnnamedArgument(1)) {
+		// Don't forget that argument 0 is the command.
+		if (! (bool) $upstreamPath = $this->_args->getUnnamedArgument(1)) {
+			// No upstreamPath specified on the command line, is there one stored?
 			$upstream = new Upstream('.');
 			$upstreamPath = $upstream->getUpstream($svn->relativePath());
 		}
@@ -23,9 +24,15 @@ class Command_Svneligible_Reintegrate extends Command_Svneligible
 			throw new Exception('You must specify a parent branch to merge into.');
 		}
 
-		echo $upstreamPath, PHP_EOL;
+		// Check that there are no eligible revisions.
+		if ((bool) $eligible = $svn->eligible($upstreamPath)) {
+			$eligibleCount = count($eligible);
+			$word = $eligibleCount == 1
+				? 'is'
+				: 'are';
+			throw new Exception('There ' . $word . ' ' . $eligibleCount . ' eligible revisions still to merge. Aborting.');
+		}
 
-		$showCommand = Command_Svneligible::factory('show');
-		$showCommand->run();
+		echo 'Reintegrating into ', $upstreamPath, PHP_EOL;
 	}
 }
