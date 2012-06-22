266ae3e2efda7e176ee96317df97266881a00e5d
Drarok Ithaqua <github@catchall.drarok.com>
2012-06-22 11:16:30 +0100

    Use the new _setUp method to see if we need to run svn update first, and do so.

--- classes/command/svneligible/show.php
+++ classes/command/svneligible/show.php
@@ -2,16 +2,25 @@
 
 /**
  * Show the revisions that are eligible for merging.
- * 
+ *
  * Shows all eligible revisions, and optionally their log messages.
  */
 class Command_Svneligible_Show extends Command_Svneligible_Filter
 {
+	protected function _setUp()
+	{
+		// Should we run update first?
+		if (Config::get('svneligible.show.auto-update', false)) {
+			echo 'Updating...', PHP_EOL;
+			$this->_svn->update();
+		}
+	}
+
 	/**
 	 * Show the revisions in $revs.
-	 * 
+	 *
 	 * @param array $revs Filtered revisions to show.
-	 * 
+	 *
 	 * @return void
 	 */
 	protected function _run($revs)
