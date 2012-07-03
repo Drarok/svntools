b5c704bf007e9479383794fd4f8262f324c19e1d
Drarok Ithaqua <github@catchall.drarok.com>
2012-07-03 14:47:06 +0100

    Whitespace cleanup.

--- classes/command/svnstash/apply.php
+++ classes/command/svnstash/apply.php
@@ -22,28 +22,28 @@ class Command_Svnstash_Apply extends Command
 	{
 		// Get the current path.
 		$path = getcwd();
-		
+
 		// Create a stash instance.
 		$stash = new Stash($path);
-		
+
 		// Grab the id from the command line.
 		$id = $this->_args->getUnnamedArgument(1, null);
-		
+
 		// Get the stash file path and name.
 		$path = $stash->getStashPath($id);
 		$name = basename($path, '.diff');
-		
+
 		echo 'Applying changes from stash \'' . $name . '\'.', PHP_EOL;
-		
+
 		$cmd = escapeshellcmd('patch') . ' 2>&1';
 		$cmd .= ' ' . escapeshellarg('-p0');
 		$cmd .= ' < ' . escapeshellarg($path);
-		
+
 		$output = array();
 		$exitCode = null;
 		exec($cmd, $output, $exitCode);
 		$output = implode(PHP_EOL, $output);
-		
+
 		if ($exitCode !== 0) {
 			throw new Exception('Failed to apply changes: ' . $output);
 		}
