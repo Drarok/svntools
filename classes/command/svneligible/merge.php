70b5b1e16a7149f5f0669b50de3293312c08eb60
Drarok Ithaqua <github@catchall.drarok.com>
2012-08-30 17:17:36 +0100

    Whitespace cleanup.

--- classes/command/svneligible/merge.php
+++ classes/command/svneligible/merge.php
@@ -2,7 +2,7 @@
 
 /**
  * Merge the revisions that are eligible.
- * 
+ *
  * Merges revisions from a given branch into the working copy.
  */
 class Command_Svneligible_Merge extends Command_Svneligible_Filter
@@ -17,9 +17,9 @@ class Command_Svneligible_Merge extends Command_Svneligible_Filter
 
 	/**
 	 * Merge all revisions in $revs into the working copy.
-	 * 
+	 *
 	 * @param array $revs Filtered revisions to merge.
-	 * 
+	 *
 	 * @return void
 	 */
 	protected function _run($revs)
