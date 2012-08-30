96085b171c00c4f2296710704f84df40cc2f33d5
Drarok Ithaqua <github@catchall.drarok.com>
2012-08-30 17:39:43 +0100

    Use the first line of the detailed help for the summary page, which saves maintaining the same text in two places.

--- classes/command/svneligible/help.php
+++ classes/command/svneligible/help.php
@@ -23,28 +23,31 @@ class Command_Svneligible_Help extends Command_Svneligible
 		$verbose = $this->_args->getNamedArgument('verbose');
 		$command = $this->_args->getUnnamedArgument(1);
 
-		// Output the general help first if in verbose mode or no command.
+		// This is the order we want to show commands in.
+		$commands = array(
+			'branches',
+			'releases',
+			'tags',
+			'path',
+			'upstream',
+			'branch',
+			'switch',
+			'diff',
+			'show',
+			'merge',
+			'reintegrate',
+			'help',
+		);
+
+		// Output the general help if in verbose mode or no specific command.
 		if ($verbose || ! $command) {
 			View::factory('svneligible/help')
 				->set('verbose', $verbose)
+				->set('commands', $commands)
 				->render();
 		}
 
 		if ($verbose && ! $command) {
-			// This is the order we want to show commands in verbose mode.
-			$commands = array(
-				'branches',
-				'releases',
-				'tags',
-				'upstream',
-				'branch',
-				'switch',
-				'diff',
-				'show',
-				'merge',
-				'reintegrate',
-				'help',
-			);
 		} elseif ($command) {
 			// Just show a single command's help.
 			$commands = array($command);
@@ -54,6 +57,11 @@ class Command_Svneligible_Help extends Command_Svneligible
 		}
 
 		foreach ($commands as $command) {
+			if ($command == 'help') {
+				// There's no additional help for the help command.
+				continue;
+			}
+
 			View::factory('svneligible/help/' . $command, 'txt')->render();
 			echo PHP_EOL, PHP_EOL;
 		}
--- views/svneligible/help.php
+++ views/svneligible/help.php
@@ -2,24 +2,36 @@ NAME
 	svneligible - Tool to assist with Subversion merges.
 
 AVAILABLE COMMANDS
-	svneligible branches
-	svneligible releases
-	svneligible tags
-	svneligible path [--stable]
-	svneligible upstream [set|remove] [<upstream>] [<alias|path>]
-	svneligible branch [<existing_path>] <new_path> [--parents] [--no-switch] [--commit=<message>]
-	svneligible switch [<path>|--stable]
-	svneligible diff [<path>|--stable] [-r|--reverse]
-	svneligible show [<path>|--stable] [--no-update] [--paths] [--author=<name>] [--initial=<rev>] [--final=<rev>] [--range=<start>:<stop>]
-	svneligible merge [<path>|--stable] [--record-only] [--author=<name>] [--initial=<rev>] [--final=<rev>] [--range=<start>:<stop>]
-	svneligible reintegrate [<path>|--stable] [--no-commit|--commit[=<message>] [--no-remove]
-	svneligible help [<command>]
+<?php
+
+$pathPrefix = SVNTOOLS_ROOT . implode(DS, array('views', 'svneligible', 'help')) . DS;
+foreach ($commands as $command) {
+	if ($command == 'help') {
+		// Output special text for the help command.
+		echo "\t", 'svneligible help [<command>]', PHP_EOL;
+		continue;
+	}
+
+	if ($command == 'upstream') {
+		// Output special text for the upstream command.
+		echo "\t", 'svneligible upstream [set|remove] [<upstream>] [<alias|path>]', PHP_EOL;
+		continue;
+	}
+
+	$file = fopen($pathPrefix . $command . '.txt', 'r');
+	echo "\t", trim(fgets($file)), PHP_EOL;
+	fclose($file);
+}
+
+?>
 
 	Note: all commands also accept the [-v|--verbose] option, which causes all Subversion commands
 	to be output to the console before they are executed.
 
 <?php if (! $verbose): ?>
 FURTHER HELP
-	To get detailed help for a single command, try 'svneligible help <command>',
-	or if you'd prefer to see all the help in one go, use 'svneligible help --verbose'.
+	To get detailed help for a single command, try 'svneligible help <command>', or
+	'svneligible <command> --help'.
+
+	If you'd prefer to see all the help in one go, use 'svneligible help --verbose'.
 <?php endif; ?>
