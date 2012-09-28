f81ef36bf8b0ccfdcb6b6380c51fafb212d93437
Drarok Ithaqua <github@catchall.drarok.com>
2012-09-28 13:09:43 +0100

    Add a 'short' display style.

---
+++ views/svneligible/log/short.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Short-style view for revisions.
+ */
+
+echo '    r', $rev, ' | ', str_pad($log->author, 10), ' | ', implode(' | ', explode(PHP_EOL, trim($log->msg))), PHP_EOL;
+
+if ($showPaths) {
+	foreach ($log->paths as $path) {
+		echo '        ', $path->action, ' ', $path->path, PHP_EOL;
+	}
+
+	echo PHP_EOL;
+}
