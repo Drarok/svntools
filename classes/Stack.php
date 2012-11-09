4f4ecfbb619d9ca3becc945c8fc1ffcbcab2845d
Drarok Ithaqua <github@catchall.drarok.com>
2012-11-09 13:55:22 +0000

    Implement a simple Stack class.

--- classes/Stack.php
+++ classes/Stack.php
@@ -9,7 +9,7 @@
 /**
  * Simple persistent stack, used for branch stack feature.
  */
-class Upstream
+class Stack
 {
 	/**
 	 * Full path to the stack file.
@@ -38,12 +38,29 @@ class Upstream
 		$this->_loadStack();
 	}
 
+	/**
+	 * Push a value onto the stack.
+	 *
+	 * @param mixed $value The value to push onto the stack.
+	 *
+	 * @return void
+	 */
 	public function push($value)
 	{
+		array_push($this->_stack, $value);
+		$this->_saveStack();
 	}
 
+	/**
+	 * Pop the last value off the stack, and return it.
+	 *
+	 * @return mixed
+	 */
 	public function pop()
 	{
+		$value = array_pop($this->_stack);
+		$this->_saveStack();
+		return $value;
 	}
 
 	/**
@@ -60,11 +77,71 @@ class Upstream
 		$this->_path = $stash->getStashDirPath() . $name;
 	}
 
-	protected function _loadStack()
-	{
-	}
-
+	/**
+	 * Save the stack to disk.
+	 *
+	 * @return void
+	 */
 	protected function _saveStack()
 	{
+		// Ensure all the files exist.
+		$this->_setupPath();
+
+		file_put_contents($this->_path, serialize($this->_stack));
+	}
+
+	/**
+	 * Load the stack from on-disk into memory.
+	 *
+	 * @return void
+	 */
+	protected function _loadStack()
+	{
+		// Set to an empty array to start with.
+		$this->_stack = array();
+
+		if (! file_exists($this->_path)) {
+			// No file? Bail.
+			return;
+		}
+
+		$contents = file_get_contents($this->_path);
+
+		if (! $contents) {
+			// Nothing in it? Bail.
+			return;
+		}
+
+		$array = unserialize($contents);
+
+		if (! is_array($array)) {
+			// Invalid contents? Bail.
+			return;
+		}
+
+		// Looking good, let's cache that.
+		$this->_stack = $array;
+	}
+
+	/**
+	 * Ensure the directory and file exist.
+	 *
+	 * @return void
+	 */
+	protected function _setupPath()
+	{
+		$dir = dirname($this->_path);
+
+		if (! is_dir($dir)) {
+			if (! mkdir($dir, 0755, true)) {
+				throw new Exception('Failed to create path: ' . $dir);
+			}
+		}
+
+		if (! file_exists($this->_path)) {
+			if (! touch($this->_path)) {
+				throw new Exception('Failed to create file: ' . $this->_path);
+			}
+		}
 	}
 }
